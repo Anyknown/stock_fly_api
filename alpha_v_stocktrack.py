51b331044dd455a6de0e0a3912712c6acc6b5068
AnyKnown <156967900+Anyknown@users.noreply.github.com>
2024-01-29 19:37:13 -0500

    Update alpha_v_stocktrack.py

--- alpha_v_stocktrack.py
+++ alpha_v_stocktrack.py
@@ -1,24 +1,37 @@
-pip install alpha_vantage
 import alpha_vantage
 import pandas as pd
-from tabulate import tabulate
 
-ts = alpha_vantage.timeseries.TimeSeries(key='QIF8N31AVQSTQK8C', output_format='pandas')
-symbols = ['TSLA', 'U', 'TWLO', 'AMZN', 'ATEN', 'ASML']
-data_dict = {}
 
-for symbol in symbols:
-    data, meta_data = ts.get_daily(symbol=symbol)
-    data_dict[symbol] = data['6. close'][0]
-  df = pd.DataFrame(data_dict.items(), columns=['Symbol', 'Price'])
+API_KEY = 'QIF8N31AVQSTQK8C'
+symbols = ['TSLA', 'U', 'TWLO', 'AMZN', 'ASML', 'ATEN']
+
+
+client = alpha_vantage.APIClient(key=API_KEY)
+
+
+intraday_data = client.get_intraday(symbol=','.join(symbols), interval='1min')
+
+
+closes = [intraday_data['Time Series (1min)'][symbol]['4. close'] for symbol in symbols]
+
+
+df = pd.DataFrame(closes, index=symbols, columns=['Close'])
+
+
 df['Purchase Price'] = None
+
+
 df['% Growth/Loss'] = None
 
-def calculate_growth(row):
+
+def calculate_growth_loss(row):
     if row['Purchase Price'] is not None:
-        return ((row['Price'] - row['Purchase Price']) / row['Purchase Price']) * 100
+        return ((row['Close'] - row['Purchase Price']) / row['Purchase Price']) * 100
     else:
         return None
 
-df['% Growth/Loss'] = df.apply(calculate_growth, axis=1)
-print(tabulate(df, headers='keys', tablefmt='grid'))
+
+df['% Growth/Loss'] = df.apply(calculate_growth_loss, axis=1)
+
+
+print(df)
